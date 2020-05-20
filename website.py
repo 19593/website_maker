5af18b0a76bd4b4ec4a536ef08e05d38c53e59b6
Joshua <jeannettevandalsum@Joshuter.local>
2020-05-21 11:32:37 +1200

    Adding SQLite and starting website

--- website.py
+++ website.py
@@ -1,21 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
-
-# Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello_world():
+    return "Hello, World!"
