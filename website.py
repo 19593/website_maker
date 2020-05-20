6e7a4cf0f9dcbf9faf06699f13b04ba27a7426be
Joshua <jeannettevandalsum@Joshuter.local>
2020-05-21 11:45:19 +1200

    rename sqlite + more website stuff

--- website.py
+++ website.py
@@ -1,7 +1,27 @@
 from flask import Flask
+import sqlite3
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+DATABASE = '/path/to/Improve.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__== '__main__':
+    app.run(debug=True)
