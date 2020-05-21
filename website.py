7d27102fc8b6475ec44713f8b82354636d91be85
Joshua <jeannettevandalsum@Joshuter.local>
2020-05-21 15:00:10 +1200

    g=Flask?

--- website.py
+++ website.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 DATABASE = '/path/to/Improve.db'
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(Flask, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(Flask, '_database', None)
     if db is not None:
         db.close()
 
