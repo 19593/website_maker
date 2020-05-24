ba21d643fed259fdb85a486971aabd1fe382ee76
Joshua <jeannettevandalsum@Joshuter.local>
2020-05-25 11:33:10 +1200

    changes

--- website.py
+++ website.py
@@ -1,27 +1,36 @@
-from flask import Flask
+from flask import Flask,g,render_template
 import sqlite3
 
 app = Flask(__name__)
 
 
-DATABASE = '/path/to/Improve.db'
+DATABASE = 'Improve.db'
 
 def get_db():
-    db = getattr(Flask, '_database', None)
+    db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(Flask, '_database', None)
+    db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contents')
 def index():
-    return 'Hello World!'
+    cursor = get_db().cursor()
+    sql = 'SELECT * FROM contents'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template('contents.html', results=results)
 
 if __name__== '__main__':
     app.run(debug=True)
